35203a369103c67a516d1da4d1bdb9d7c72461e4
Sunniva Indrehus <sunniva.indrehus@gmail.com>
2022-12-08 22:16:51 +0100

    forced bad formatting for example

--- factorial.py
+++ factorial.py
@@ -1,7 +1,11 @@
-def factorial(n: int) -> int:
-    if n < 2:
-        return 1
-    return n * factorial(n - 1)
+from pathlib import Path
+import math 
 
+l = 42 
+def factorial(
+    n: int) -> int        :
+    if n <          2:
+        return 1
+    return           n * factorial(n - 1)
 
 print(f"value is: {factorial(4)}")

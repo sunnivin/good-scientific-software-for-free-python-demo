78020423064d0ca028b95d430cf071d5b80ada94
Sunniva Indrehus <sunniva.indrehus@ngi.no>
2022-12-09 11:46:34 +0100

    func: new example for variable call

--- factorial.py
+++ factorial.py
@@ -1,10 +1,11 @@
-my_variable = 42
+from pathlib import Path
+import math 
 
-
-def factorial(n: int) -> int:
-    if n < 2:
+l = 4 
+def factorial(
+    n: int) -> int        :
+    if n <          2:
         return 1
-    return n * factorial(n - 1)
+    return           n * factorial(n - 1)
 
-
-print(f"value is: {factorial(6)}")
+print(f"value is: {factorial(l)}")
